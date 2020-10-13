d62f2fa2498796b06c8c6d08980e500a52fe8f66
ronyefreitas <ronye.freitas@gmail.com>
2020-10-13 06:48:46 -0300

    New comments on code.

--- guinvest.py
+++ guinvest.py
@@ -11,37 +11,52 @@ import numpy as np
 import pmdarima as pm
 import requests
 from bs4 import BeautifulSoup
-from urllib.request import urlopen
+
 
 today = dt.date.today()
 tomorrow = today + dt.timedelta(days=1)
 
-@st.cache(persist=True)
+
+@st.cache(persist=True)  # avoid realoadig data constantly
 def load_tickers():
+    """ This function returns a pandas DataFrame with all the tickers available
+    for the Brazilian stocks market using as source the webpage
+    https://www.fundamentus.com.br/resultado.php"""
 
     agent = {"User-Agent": "Mozilla/5.0"}
     page = requests.get('https://www.fundamentus.com.br/resultado.php',
                         headers=agent)
-
+    # Getting the page content.
     soup = BeautifulSoup(page.content, "html.parser")
+    # In order to find what you want from the webpage, you can open it in a
+    # chrome web browser, open the inspect element tool and figure out the id
+    # and/or tags of the desired data.
     soup.find_all(id="resultado")
-    table_fundamentus = soup.find_all(id="resultado")[0]
+    table_fundamentus = soup.find_all(id="resultado")[0]  # Getting the table
     tickers_list = []
+    # Looking into the table and retrieving only the tickers
     for row in table_fundamentus.find_all('tr')[1:]:
         tickers_list.append(row.find(class_='tips').find('a').get_text())
 
     df = pd.DataFrame({'ticker': tickers_list})
+    # in order to use the yfinance library to retrieve Stocks info directly
+    # from Yahoo! Finance it is necessary to include '.SA' at the end of each
+    # ticker, because that is the format required by yfinance.
     df['y_ticker'] = df['ticker'].apply(lambda x: str(x) + '.SA')
 
     return df
 
 @st.cache(persist=True)
 def load_historical_data(tickers, start_date, end_date, df_type='prices'):
-
+    """ This function returns a pandas DataFrame with the closing prices of the
+    given tickers between a start date and and date (not included).
+    When df_type='ret' option is selected, the function returns the percent
+    change related to the previous closing price as well."""
     yf_tickers = []
     for ticker in tickers:
+        # Creating a list of yfinance tikers objects
         yf_tickers.append([yf.Ticker(ticker), ticker])
-
+    # Formarting date to match yfinance library requirements
     start_date = start_date.strftime('%Y-%m-%d')
     end_date = end_date.strftime('%Y-%m-%d')
 
@@ -51,8 +66,11 @@ def load_historical_data(tickers, start_date, end_date, df_type='prices'):
     prices_cols = []
     for yf_ticker, str_ticker in yf_tickers:
         df_history = yf_ticker.history(start=start_date, end=end_date)
+        # Sorting the data in order to show the latest valus first
         df_history.sort_index(ascending=False, inplace=True)
         df_all[str_ticker + ' Close'] = df_history['Close']
+
+        # Calculating the return values based on previous closing price.
         df_all[str_ticker + ' Close D-1'] = (df_all[str_ticker
                                                     + ' Close'].shift(-1))
 
@@ -68,6 +86,8 @@ def load_historical_data(tickers, start_date, end_date, df_type='prices'):
 
     df_all.index = df_all.index.strftime('%Y-%m-%d')
 
+    # Return a DataFrame with closing prices and percent return or with
+    # closing prices only.
     if df_type == 'prices':
         return df_all[prices_cols]
     elif df_type == 'ret':
@@ -102,6 +122,7 @@ def make_table(df_prices):
 
 st.cache(persist=True)
 def make_df_pred_ARIMA(df_prices, steps):
+    """ This function creates the DataFrame which has the tickers"""
     df_true_values = df_prices.sort_index(ascending=True)
     forecasts = pd.DataFrame(index=[f'(ARIMA) D + {i + 1}' for i in range(steps)])
     for col in df_true_values.columns:
